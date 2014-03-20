dd3ea7264bf7978cab63b6a01a1af2e6092148d7
Bruce Crevensten <bruce.crevensten@gmail.com>
2014-03-19 18:59:55 -0800

    adds cookbooks to support vagrant provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,30 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant::Config.run do |config|
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
   config.vm.box = "ubuntu-12.04-32bit"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.synced_folder "./", "/home/vagrant/ardupilot"
+  
+  config.vm.provider :virtualbox do |vb|
 
-  config.vm.share_folder("ardupilot", "/home/vagrant/ardupilot", ".")
+    # Otherwise the compile will go into swap, making things slow
+    vb.memory = 3072
+    vb.cpus = 2
 
-  # Allow symlinks
-  config.vm.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/cross-compiler", "1"]
-  # Otherwise the compile will go into swap, making things slow
-  config.vm.customize ["modifyvm", :id, "--memory", "2048"]
+  end
+
+  config.vm.provision "chef_solo" do |chef|
+    chef.add_recipe "ardupilot"
+  end
+
+  # Allow forwarding of X sessions for --map and other MAVProxy plugin options.
+  config.ssh.forward_x11 = true
+
+  # Allow symlinks... used at all?
+  # config.vm.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/cross-compiler", "1"]
+    
 end
-
---
+++ cookbooks/ardupilot/recipes/default.rb
@@ -0,0 +1,63 @@
+# System packages needed to install software.
+include_recipe "apt"
+
+[
+  # Core/common dependencies
+  'make',
+  'git',
+  'arduino-core',
+  'curl',
+  'gawk',
+  'g++',
+
+  # PX4 deps here
+  'python-serial',
+  'openocd',
+  'flex',
+  'bison',
+  'libncurses5-dev',
+  'autoconf',
+  'texinfo',
+  'build-essential',
+  'libftdi-dev',
+  'libtool',
+  'zlib1g-dev',
+  'zip',
+  'genromfs' ,
+
+  # SITL/autotest dependencies here
+  'dos2unix',
+  'ccache',
+  'python-opencv',
+  'python-wxgtk2.8',
+  'gnome-terminal'
+  ].each do |pkg|
+  package pkg
+end
+
+# Python dependencies
+include_recipe "python"
+python_pip "pymavlink"
+python_pip "mavproxy"
+python_pip "pexpect"
+python_pip "matplotlib"
+
+
+# Environmental modifications
+# ruby_block "include-bashrc-user" do
+#   block do
+#     file = Chef::Util::FileEdit.new("#{ENV['HOME']}/.bashrc")
+#     file.insert_line_if_no_match(
+#       "# Include paths needed to run Arudpilot code/tests\n",
+#       "export PATH=/usr/lib/ccache:$PATH\n",
+#       "export PATH=$PATH:$HOME/GitHub/ardupilot/Tools/autotest\n",
+#       "export PATH=$PATH:$HOME/GitHub/mavlink/pymavlink\n",
+#       "export PATH=$PATH:$HOME/GitHub/MAVProxy\n"
+#     )
+#     file.write_file
+#   end
+# end
+
+
+
+
