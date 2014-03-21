92c06bf54b8492d816fdc712280909081b004853
Bruce Crevensten <bruce.crevensten@gmail.com>
2014-03-20 20:37:17 -0800

    Further Vagrant/Chef tweaks

--- cookbooks/ardupilot/recipes/default.rb
+++ cookbooks/ardupilot/recipes/default.rb
@@ -9,6 +9,8 @@ include_recipe "apt"
   'curl',
   'gawk',
   'g++',
+  'dos2unix',
+  'ccache',
 
   # PX4 deps here
   'python-serial',
@@ -24,10 +26,8 @@ include_recipe "apt"
   'zlib1g-dev',
   'zip',
   'genromfs' ,
-
-  # SITL/autotest dependencies here
-  'dos2unix',
-  'ccache',
+  
+  # Some Python dependencies are easier to build this way than with pip.
   'python-opencv',
   'python-wxgtk2.8',
   'python-matplotlib',
@@ -42,22 +42,20 @@ python_pip "pymavlink"
 python_pip "mavproxy"
 python_pip "pexpect"
 
-command "cd ardupilot/ArduCopter && make configure"
+# Trigger initial generation of a config.mk file.
+execute "cd /home/vagrant/ardupilot/ArduCopter && make configure"
 
 # Environmental modifications
-# ruby_block "include-bashrc-user" do
-#   block do
-#     file = Chef::Util::FileEdit.new("#{ENV['HOME']}/.bashrc")
-#     file.insert_line_if_no_match(
-#       "# Include paths needed to run Arudpilot code/tests\n",
-#       "export PATH=/usr/lib/ccache:$PATH\n",
-#       "export PATH=$PATH:$HOME/GitHub/ardupilot/Tools/autotest\n",
-#       "export PATH=$PATH:$HOME/GitHub/mavlink/pymavlink\n",
-#       "export PATH=$PATH:$HOME/GitHub/MAVProxy\n"
-#     )
-#     file.write_file
-#   end
-# end
+ruby_block "include-bashrc-user" do
+  block do
+    file = Chef::Util::FileEdit.new("#{ENV['HOME']}/.bashrc")
+    file.insert_line_if_no_match(
+      "# Include paths needed to run Arudpilot code and tests",
+      "\n\n# Include paths needed to run Arudpilot code and tests\nexport PATH=/usr/lib/ccache:$PATH",
+    )
+    file.write_file
+  end
+end
 
 
 
